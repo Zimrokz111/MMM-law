96d607e2844c1ce80579185573e32a35bda90abb
Nishan Mozumder <arosh019@gmail.com>
2017-06-18 09:37:51 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// Author's Js..
+
+//Post Slider Js
+$(document).ready(function() {
+     
+      $("#owl-demo").owlCarousel({
+     
+          autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3]
+     
+      });
+     
+});
+
+//Bootstrap Nav
+
+$('.navbar-default .navbar-nav > li.dropdown').hover(
+    function() {
+      $('ul.dropdown-menu', this).stop(true, true).fadeIn('slow');
+      $(this).addClass('open');
+    }, function() {   
+      $('ul.dropdown-menu', this).stop(true, true).fadeOut('slow');
+      $(this).removeClass('open');
+    }
+);
+
+//Video Background Js
+
+$('#my-video').backgroundVideo();
+
+//Progress Bar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
